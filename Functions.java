f02475a0f9cdb92107f39b722fa734a90f5ba907
rodrigoschaffer <rodrigoschafferc@gmail.com>
2023-08-23 22:45:15 -0300

    Adicionei o menu.

--- Functions.java
+++ Functions.java
@@ -1,3 +1,37 @@
 public class Functions {
-    
+    public static String convertNumber(int i) {
+        String convertedNumber = "";
+        
+        switch (i) {
+
+            case 1:
+                convertedNumber = "I"; 
+                break;
+            case 2:
+                convertedNumber = "II"; 
+                break;
+            case 3:
+                convertedNumber = "III"; 
+                break;
+            case 4:
+                convertedNumber = "IV"; 
+                break;
+            case 5:
+                convertedNumber = "V";
+                break;
+            case 6:
+                convertedNumber = "VI";
+                break;
+            case 7:
+                convertedNumber = "VII";
+                break;
+            case 8:
+                convertedNumber = "VIII"; 
+                break;
+            case 9:
+                convertedNumber = "IX"; 
+                break;
+        }
+        return convertedNumber;
+    }
 }
---
+++ Menu.java
@@ -0,0 +1,8 @@
+public class Menu {
+    public static void menu() {
+
+        while(true) {
+
+        }
+    }
+}
